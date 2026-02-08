427634700484931973eec29d838ec796b7baec1a
dharmeshkumarkhairnar1715 <dharmeshkumarkhairnar@gmail.com>
2026-02-08 18:50:09 +0530

    repo name added in config file

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dharmeshkumarkhairnar1715/',   // 👈 ADD THIS
+  base: '/Gift/',   // 👈 ADD THIS
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
